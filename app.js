353def270168a335e9e008139b60d8fa01bae0b1
Siddhi-244 <siddhibhanushali1234@gmail.com>
2021-10-29 14:48:33 +0530

    otp generation done

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const sgMail = require('@sendgrid/mail');
-
 sgMail.setApiKey("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");
 function sendOtp(receiver) {
+    const otp= Math.floor(Math.random()*1000000);
     
 const msg = {
     to: `${receiver}`,
-    from: `${process.env.EMAIL}`,
-    subject: 'Sending with Twilio SendGrid is Fun',
-    text: `The Otp is`,
-    html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+    from: 'mrudulkolambe02@gmail.com',
+    subject: 'OTP Verification',
+    text: `The Otp is ${otp}`,
+    html: `<strong>The Otp is ${otp} and easy to do anywhere, even with Node.js</strong>`,
 };
 sgMail.send(msg)
     .then(function () {
@@ -18,4 +18,4 @@ sgMail.send(msg)
         console.log(error.message);
     })
 }
-module.exports = send;
+module.exports = sendOtp;
